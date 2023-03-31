11391b3ae21bc60e65b07379084685221e077180
osw6858 <osw7875@naver.com>
2023-03-31 17:06:48 +0900

    '영화진흥api연결'

--- src/movies/movies.js
+++ src/movies/movies.js
@@ -1,8 +1,27 @@
 import "./movies.css";
 import { Empty } from "antd";
+import React from "react";
+import axios from "axios";
 
 function Movies() {
   const searchString = 1;
+  const [movies, setMovies] = React.useState([]);
+  const url =
+    "http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=7e9a703145ec25f8dc300521b3384744";
+
+  React.useEffect(function () {
+    axios
+      .get(url)
+      .then(function (result) {
+        console.log("결과입니다", result);
+        const movies = result.data.movieListResult.movieList;
+        setMovies(movies);
+      })
+      .catch(function (err) {
+        console.log("에러발생", err);
+      });
+  }, []);
+
   if (searchString === null) {
     return (
       <div className="no-search">
@@ -13,23 +32,27 @@ function Movies() {
   return (
     <main>
       <section className="video-grid">
-        <div className="video-preview">
-          <div className="thumbnail-row">
-            <img
-              className="thumbnail"
-              src="assets/images/thumbnails/avengers.jpg"
-              alt="영화포스터"
-            />
-            <div className="video-time">러닝타임</div>
-          </div>
-          <div className="video-info-grid">
-            <p className="video-title">제목 : 어벤져스</p>
-            <p className="video-author">감독 : 몰라</p>
-            <p className="video-stats">
-              관람객 : 00명 &#183; 개봉일 : 2023/2/23
-            </p>
-          </div>
-        </div>
+        {movies.map((movies, index) => {
+          return (
+            <div key={index} className="video-preview">
+              <div className="thumbnail-row">
+                <img
+                  className="thumbnail"
+                  src="assets/images/thumbnails/movieLogo2.jpg"
+                  alt="영화포스터"
+                />
+                <div className="video-time">{movies.prdtStatNm}</div>
+              </div>
+              <div className="video-info-grid">
+                <p className="video-title"> {movies.movieNm}</p>
+                <p className="video-author">장르 : {movies.genreAlt}</p>
+                <p className="video-stats">
+                  국가 : {movies.repNationNm} &#183; 개봉일 : {movies.openDt}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </main>
   );
