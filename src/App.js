c6a7a3f63a61c0507315f417e6ace6942b426862
osw6858 <osw7875@naver.com>
2023-04-17 18:23:05 +0900

    '모바일화면깨짐수정'

--- src/App.js
+++ src/App.js
@@ -59,37 +59,39 @@ function App() {
                             open={open}
                             extra={<Space > <Button onClick={onClose}>닫기</Button>
                         </Space>}>
-                        <h2>페이지 소개</h2>
-                           <ul>
-                           <li>영화진흥회 api를 이용한 간단한 영화정보 검색 페이지</li>
+                            <h2>페이지 소개</h2>
+                            <ul>
+                                <li>영화진흥회 api를 이용한 간단한 영화정보 검색 페이지</li>
+                                <br/>
+                                <li>카카오 로그인api이용 로그인 상태일때만 성인물 표시</li>
+                                <br/>
+                                <li>React를 이용하여 SPA방식으로 구현</li>
+                                <br/>
+                                <li>디바운싱 적용하여 검색시 과도한 api호출 방지</li>
+                                <br/>
+                                <li>antd이용 레이아웃 구성</li>
+                                <br/>
+                                <li>
+                                    <a href="https://osw6858.github.io/Go-movie/">각 부분 문제 해결 과정</a>
+                                </li>
+                            </ul>
                             <br/>
-                            <li>카카오 로그인api이용 로그인 상태일때만 성인물 표시</li>
-                            <br/>
-                            <li>React를 이용하여 SPA방식으로 구현</li>
-                            <br/>
-                            <li>디바운싱 적용하여 검색시 과도한 api호출 방지</li>
-                            <br/>
-                            <li>antd이용 레이아웃 구성</li>
-                            <br/>
-                            <li><a href="https://osw6858.github.io/Go-movie/">각 부분 문제 해결 과정</a></li>
-                          </ul>
-                          <br/>
                             <h2>제작과정</h2>
-                          <ul>
-                            <li>총 재작 기간 14일</li>
+                            <ul>
+                                <li>총 재작 기간 14일</li>
+                                <br/>
+                                <li>3/30 - 레이아웃 구상</li>
+                                <br/>
+                                <li>3/31 ~ 4/1 - 영화진흥회api 연결, 결과 목록 페이징</li>
+                                <br/>
+                                <li>4/2 ~ 4/7 - 검색기능 구현</li>
+                                <br/>
+                                <li>4/8 ~ 4/10 - 카카오 로그인 api 연결</li>
+                                <br/>
+                                <li>4/11 ~ 4/14 - 검색카테고리 추가 및 버그 수정</li>
+                            </ul>
                             <br/>
-                            <li>3/30 - 레이아웃 구상</li>
-                            <br/>
-                            <li>3/31 ~ 4/1 - 영화진흥회api 연결, 결과 목록 페이징</li>
-                            <br/>
-                            <li>4/2 ~ 4/7 - 검색기능 구현</li>
-                            <br/>
-                            <li>4/8 ~ 4/10 - 카카오 로그인 api 연결</li>
-                            <br/>
-                            <li>4/11 ~ 4/14 - 검색카테고리 추가 및 버그 수정</li>
-                          </ul>
-                          <br/>
-                           
+
                         </Drawer>
                     </div>
                     <div>
@@ -106,7 +108,7 @@ function App() {
                     </div>
                 </div>
                 <div className="middle-section">
-                   
+
                     <input
                         className="search-bar"
                         type="text"
@@ -129,31 +131,27 @@ function App() {
 
                 </div>
                 <div className="right-section">
-                    <div className="select-container">
-                        <div className="login">
-                            {
-                                ACCESS_TOKEN !== null
-                                    ? (
-                                        <button
-                                            onClick={() => {
-                                                navigate("/logout");
-                                            }}
-                                            className="login-out-button">
-                                            로그아웃
-                                        </button>
-                                    )
-                                    : (
-                                        <button
-                                            onClick={() => {
-                                                navigate("/login");
-                                            }}
-                                            className="login-out-button">
-                                            로그인
-                                        </button>
-                                    )
-                            }
-                        </div>
-                    </div>
+                        {
+                            ACCESS_TOKEN !== null
+                                ? (
+                                    <button
+                                        onClick={() => {
+                                            navigate("/logout");
+                                        }}
+                                        className="login-out-button">
+                                        로그아웃
+                                    </button>
+                                )
+                                : (
+                                    <button
+                                        onClick={() => {
+                                            navigate("/login");
+                                        }}
+                                        className="login-out-button">
+                                        로그인
+                                    </button>
+                                )
+                        }
                 </div>
             </header>
             <div className="body">
@@ -173,12 +171,8 @@ function App() {
                     <Route path="/logout" element={<LogOut></LogOut>}/>
                     <Route path="/kakaologout" element={<KakaoLogout></KakaoLogout>}/>
                 </Routes>
-
-                <Collapse size="large">
-                    <Panel header="일일 박스오피스 순위" key="1" className="boxoffice-collapse">
-                        <Boxoffice></Boxoffice>
-                    </Panel>
-                </Collapse>
+                <hr className="hr-line"></hr>
+                <Boxoffice></Boxoffice>
 
             </div>
             <footer >
